f4fec3f40bc04949ee6decf0daffe22e09e6f863
Felipe Botero <frpbotero@gmail.com>
2023-01-31 14:40:13 -0400

    Instalação do MongoDB | Rota Post
    * Instalação do MongoDB
    *Criada rota para cadastro de novos livros

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,49 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+// Connection URL
+const url = process.env.URL;
+const client = new MongoClient(url);
+const dbName = process.env.BDNAME;
 
-app.listen(3000);
+async function main() {
+  console.log("Conecntando ao bando de dados.");
+  await client.connect();
+  console.log("Conectado com sucesso no banco de dados.");
+  const db = client.db(dbName);
+  const livrosCollection = db.collection("Livros");
+
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.get("/", function (req, res) {
+    res.send("Hello World");
+  });
+  // ==============CRUD LIVROS==============
+  app.post("/livros", async function (req, res) {
+    const book = req.body;
+
+    await livrosCollection.insertOne(book);
+
+    if (!book || !book.name) {
+      res.status(400).send({
+        message: "Livro não cadastrado",
+      });
+    } else {
+      res.send({
+        message: "Livro cadastrado com sucesso",
+      });
+    }
+  });
+
+  app.listen(3000, () => {
+    console.log(`Servidor rodando na porta 3000`);
+  });
+}
+
+main();
