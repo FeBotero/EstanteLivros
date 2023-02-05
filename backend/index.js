36b8dac357e4224e9e22a178441308931f9ab871
Felipe Botero <frpbotero@gmail.com>
2023-02-04 20:01:47 -0400

    Criado rota para usuarios
    CRUD para usuarios

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ async function main() {
   const db = client.db(dbName);
   const livrosCollection = db.collection("Livros");
   const reservaCollection = db.collection("reserva");
+  const usersCollection = db.collection("users");
 
   const app = express();
 
@@ -81,10 +82,10 @@ async function main() {
 
     await livrosCollection.deleteOne({ _id: new ObjectId(id) });
     res.send({
-      message: "Livro excluído com sucesso",
+      message: "Livro excluído com sucesso!",
     });
   });
-
+  // ======================Reservas=====================
   //   ----------------------CREATE------------------
   app.post("/reservas", async function (req, res) {
     const booking = req.body;
@@ -93,11 +94,11 @@ async function main() {
 
     if (!booking || !booking.bookingName) {
       res.status(400).send({
-        message: "Reserva não cadastrada",
+        message: "Reserva não cadastrada!",
       });
     } else {
       res.send({
-        message: "Reserva cadastrada com sucesso",
+        message: "Reserva cadastrada com sucesso!",
       });
     }
   });
@@ -116,7 +117,7 @@ async function main() {
     });
     if (!bookings) {
       res.status(400).send({
-        message: "Reserva não encontrada",
+        message: "Reserva não encontrada!",
       });
     } else {
       res.send(bookings);
@@ -139,7 +140,66 @@ async function main() {
 
     await reservaCollection.deleteOne({ _id: new ObjectId(id) });
     res.send({
-      message: "Reserva excluída com sucesso",
+      message: "Reserva excluída com sucesso!",
+    });
+  });
+
+  // ======================Users=====================
+  //   ----------------------CREATE------------------
+  app.post("/users", async function (req, res) {
+    const user = req.body;
+
+    await usersCollection.insertOne(user);
+
+    if (!user || !user.name) {
+      res.status(400).send({
+        message: "Usuário não cadastrado!",
+      });
+    } else {
+      res.send({
+        message: "Usuário cadastrado com sucesso!",
+      });
+    }
+  });
+
+  //   ----------------------READ------------------
+  app.get("/users", async function (req, res) {
+    const user = await usersCollection.find().toArray();
+
+    res.send(user);
+  });
+  //   ----------------------READ BY ID------------------
+  app.get("/users/:id", async function (req, res) {
+    const id = req.params.id;
+    const user = await usersCollection.findOne({
+      _id: ObjectId(id),
+    });
+    if (!user) {
+      res.status(400).send({
+        message: "Usuário não encontrado!",
+      });
+    } else {
+      res.send(user);
+    }
+  });
+
+  //   ----------------------UPDATE------------------
+  app.put("/users/:id", async function (req, res) {
+    const id = req.params.id;
+
+    const user = req.body;
+
+    await usersCollection.updateOne({ _id: ObjectId(id) }, { $set: user });
+
+    res.send({ message: "Usuário atualizado com sucesso!" });
+  });
+  //   ----------------------DELETE------------------
+  app.delete("/users/:id", async function (req, res) {
+    const id = req.params.id;
+
+    await usersCollection.deleteOne({ _id: new ObjectId(id) });
+    res.send({
+      message: "Usuário excluído com sucesso!",
     });
   });
 
